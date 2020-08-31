74aed571decf90af8dcebbf59cbe0e8e43247fac
Sarah <saranderson13@gmail.com>
2020-08-31 19:42:37 -0400

    add links to battles in progress to user pages

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -16,4 +16,31 @@ module UsersHelper
     render 'layouts/cu_profile_links' if logged_in? && (current_user.id == params[:id].to_i || current_user.admin_status)
   end
 
+
+  def user_battles_in_progress
+    viewing = User.find(params["id"])
+    if viewing.protag_battles.in_progress.length > 0
+      in_progress = <<~HEREDOC
+      <div class='user_bip_title'>#{viewing.protag_battles.in_progress.count} battles in progress</div>
+      <div class='bip_links'>#{user_battles_in_progress_list}</div>
+      HEREDOC
+      return in_progress.html_safe
+    else
+      "<div class='user_bip_title'>No battles in progress.</div>".html_safe
+    end
+  end
+
+  def user_battles_in_progress_list
+    viewing = User.find(params["id"])
+    battle_list = ""
+    for b in viewing.protag_battles.in_progress do
+      protag = Character.find(b.protag_id)
+      antag = Character.find(b.antag_id)
+      battle_list += "<a href='/battles/#{b.id}' class='bip_link'>#{protag.supername} vs. #{antag.supername}</a>"
+    end
+
+    return battle_list
+  end
+
 end
+
