8db10ffeee6bc287c122d127238ad45ca4f641ad
Sarah <saranderson13@gmail.com>
2020-10-22 15:46:52 -0400

    set up model for NewsItem; add 'News' link to logged_in navbar

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,4 +1,10 @@
 class NewsItem < ApplicationRecord
 
-    
+    validates :title, :description, presence: true
+    validates :title, length: { maximum: 150, minimum: 10 }
+    validates :description, length { maximum: 500, minimum: 100 }
+    validates :homepage, inclusion: { in: [true, false] }
+
+    scope :newsfeed, -> { where(homepage: true) }
+
 end
