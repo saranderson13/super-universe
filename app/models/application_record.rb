274d2af855c94f5c22a0c9d49b9bb635d794fe08
Sarah <saranderson13@gmail.com>
2020-11-12 18:56:16 -0500

    modify #ago method to display weeks if days > 7, in both pre and post 1 year.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -13,12 +13,19 @@ class ApplicationRecord < ActiveRecord::Base
         return "#{pluralize(diff.in_minutes.to_i, 'minute')} ago"
     elsif diff.in_hours < 24
         return "#{pluralize(diff.in_hours.to_i, 'hour')} ago"
-    elsif diff.in_days < 365
+    elsif diff.in_days < 7
         return "#{pluralize(diff.in_days.to_i, 'day')} ago"
+    elsif diff.in_weeks < 52
+        return "#{pluralize(diff.in_weeks.to_i, 'week')} ago"
     else
         years = diff.in_years.to_i
         days = (diff.in_days - (years * 365)).to_i
-        return "#{pluralize(years, 'year')}, #{pluralize(days, 'day')} ago"
+        weeks = (diff.in_weeks - (years * 52)).to_i
+        if days > 7
+          return "#{pluralize(years, 'year')}, #{pluralize(weeks, 'week')} ago"
+        else
+          return "#{pluralize(years, 'year')}, #{pluralize(days, 'day')} ago"
+        end
     end
   end
 
