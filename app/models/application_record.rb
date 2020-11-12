9d29dabe0aaa2b6dd7a125a20ad33aa8f8d2abe4
Sarah <saranderson13@gmail.com>
2020-11-12 17:42:51 -0500

    move #ago to ApplicationRecord (so all objects can use it)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,22 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def ago(createdAtOrUpdatedAt)
+    # call with "created_at" OR "updated_at"
+    # to test in terminal, run "include ActionView::Helpers::TextHelper"
+    # (needed because #pluralize is not known without being included)
+
+    diff = TimeDifference.between(Time.now.utc, Time.parse(self.send(createdAtOrUpdatedAt).to_s))
+
+    if diff.in_seconds < 60
+        return "#{pluralize(diff.in_seconds.to_i, 'second')} ago"
+    elsif diff.in_minutes < 60
+        return "#{pluralize(diff.in_minutes.to_i, 'minute')} ago"
+    elsif diff.in_hours < 24
+        return "#{pluralize(diff.in_hours.to_i, 'hour')} ago"
+    else
+        return "#{pluralize(diff.in_days.to_i, 'day')} ago"
+    end
+  end
+
 end
--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -7,21 +7,4 @@ class NewsItem < ApplicationRecord
 
     scope :newsfeed, -> { where(homepage: true).reverse() }
 
-
-    def ago(createdAtOrUpdatedAt)
-        # call with "created_at" OR "updated_at"
-
-        diff = TimeDifference.between(Time.now.utc, Time.parse(self.send(createdAtOrUpdatedAt).to_s))
-
-        if diff.in_seconds < 60
-            return "#{pluralize(diff.in_seconds.to_i, 'second')} ago"
-        elsif diff.in_minutes < 60
-            return "#{pluralize(diff.in_minutes.to_i, 'minute')} ago"
-        elsif diff.in_hours < 24
-            return "#{pluralize(diff.in_hours.to_i, 'hour')} ago"
-        else
-            return "#{pluralize(diff.in_days.to_i, 'day')} ago"
-        end
-    end
-
 end
