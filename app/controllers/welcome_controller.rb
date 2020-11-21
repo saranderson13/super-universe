4b4186abf1928b5438400b254473b8fbd1496918
Sarah <saranderson13@gmail.com>
2020-11-20 20:44:46 -0500

    add top_supers_rank to home page (adjust styling to fit)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,8 @@ class WelcomeController < ApplicationController
 
   def home
     @recent = Battle.recent_battles
-    @leaders_records = Character.leader_board("protag_rank")
+    @top_supers = Character.leader_board("top_supers_rank")
+    @protag_records = Character.leader_board("protag_rank")
     @antag_records = Character.leader_board("antag_rank")
     @news = NewsItem.newsfeed
     
