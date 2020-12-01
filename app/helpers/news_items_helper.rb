a2414be9f58b34ca74707f19c7b8a2c5a86a6a35
Sarah <saranderson13@gmail.com>
2020-12-01 17:40:45 -0500

    add error handling for news_item new & edit forms; transfer redundant fields to partial.

--- app/helpers/news_items_helper.rb
+++ app/helpers/news_items_helper.rb
@@ -1,2 +1,13 @@
 module NewsItemsHelper
+
+    def news_form_error(field)
+        case field
+        when :title
+            "!! News title length is invalid. Must be between 10 - 150 characters. You have used #{@news_item.title.length}/500 characters." if !@news_item.errors[:title].empty?
+        when :description
+            "!! News description length is invalid. Must be between 50 - 500 characters. You have used #{@news_item.description.length}/500 characters." if !@news_item.errors[:description].empty?
+        end
+    end
+
+
 end
